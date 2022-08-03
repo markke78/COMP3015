2e6cbf0d6d31a00f6fbee3660e7f1a4bf4f716eb
Christian Fenn <christianhfenn@gmail.com>
2022-08-03 02:05:30 -0700

    Updated README, moved comment info

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # PHPLoops
+
+## Run from terminal:
+
+```
+php main.php
+```
+
+### Mini Challenge
+
+What if we have a show with multiple associated dates?
+```
+$shows = [
+     'Seinfeld' => ['July 5th, 1989 - May 14th, 1998'],
+     'Curb Your Enthusiasm' => ['October 15th, 2000 - Current'],
+     'The Simpsons' => ['December 17, 1989 - Current'],
+     'Futurama' => ['March 28, 1999 - August 10, 2003', 'March 23, 2008 - September 4, 2013'],
+];
+```
+Can we loop through this? Try it. Notice each value in `$shows` is an array now, instead of a string.
--- main.php
+++ main.php
@@ -21,12 +21,3 @@ foreach ($shows as $name => $airedDates) {
 }
 
 echo PHP_EOL . "Program finished running." . PHP_EOL;
-
-// What if we have a show with multiple associated dates? 
-// $shows = [
-//     'Seinfeld' => ['July 5th, 1989 - May 14th, 1998'],
-//     'Curb Your Enthusiasm' => ['October 15th, 2000 - Current'],
-//     'The Simpsons' => ['December 17, 1989 - Current'],
-//     'Futurama' => ['March 28, 1999 - August 10, 2003', 'March 23, 2008 - September 4, 2013'] # <-- nested array
-// ];
-// Can we loop through this? Try it. Notice each value is an array now.
